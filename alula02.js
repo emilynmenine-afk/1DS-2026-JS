0cdbbd259561cb6dbdb037687d02982b8387fa35
emizinha <emilyn.menine@escola.pr.gov.br>
2026-05-12 10:27:58 -0300

    Add files via upload

--- alula02.js
+++ alula02.js
@@ -8,3 +8,24 @@ let salarioPessoa = 1500;
 let salarioBonus = 100
 
 let salarioTotal = salarioBonus + salarioPessoa;
+
+console.log( pessoa );
+console.log( idadePessoa );
+console.log( alturaPessoa );
+console.log( statusPessoa );
+console.log( salarioPessoa );
+console.log( salarioBonus );
+console.log( salarioTota );
+
+//UNINDO TOTAS AS INFORMAÇÔES EM UM TEXTO:
+console.log(
+'O nome digitado foi: '+pessoa+
+' a idade digitada foi: '+idadePessoa+
+' a altura digitada foi: '+ alturaPessoa+
+' o status digitada da pessoa é: '+statusPessoa+
+' o salario da pessoa é: '+salarioPessoa+
+' e o salario bonus é: '+salarioBonua+
+' e o salario total é: '+salarioTotal+
+
+
+)
