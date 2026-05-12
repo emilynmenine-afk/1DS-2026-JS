38699621f41f46cf8284a45570200d2b35bce7c2
emizinha <emilyn.menine@escola.pr.gov.br>
2026-05-12 08:32:21 -0300

    Add files via upload

--- aula01.js
+++ aula01.js
@@ -1,1 +1,10 @@
-alert("0 carpa copim fica batendo no amiguinho!!!")
+//alert("0 carpa copim fica batendo no amiguinho!!!")
+
+console.log("Primeiro código em JS");
+console.log(4+5);
+console.log( 3 + 4;)
+console.log( true );
+console.log( 3.89 );
+
+console.log( "Emilyn" + " " +1,60+" "+ true );
+
